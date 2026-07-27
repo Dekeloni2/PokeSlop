92ae2ed10fdf85d2c47ab1cddf5f144dd7654792
Dekeloni2 <dekelmail34@gmail.com>
2026-07-28 02:36:46 +0300

    shrek is love

--- FinalProject/States/OverworldState.cs
+++ FinalProject/States/OverworldState.cs
@@ -250,7 +250,7 @@ namespace FinalProject.States
             string teachersDir = Path.GetFullPath(
                 Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "Content", "Teachers"));
             // swap this filename to whoever you're testing (dorbendor.json, yakir.json, substitute.json)
-            TeacherStats stats = TeacherLoader.Load(Path.Combine(teachersDir, "dorbendor.json"));
+            TeacherStats stats = TeacherLoader.Load(Path.Combine(teachersDir, "yakir.json"));
             var teacher = new Teacher(stats);
 
             // the Undertale-style intro plays first, then hands off to the battle.
